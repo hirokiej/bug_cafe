7fb747b0ae1b07fc252bdc77989d3f61cc3866e3
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-01-22 20:14:04 -0500

    デバッグを使用してプログラムを修正しました

--- cafe.rb
+++ cafe.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'debug'
 
 DRINKS = [
   { name: 'コーヒー', price: '300' },
@@ -19,7 +20,9 @@ def take_order(menus)
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
   print '>'
-  order_number = gets.to_i
+  # 正しい配列出力にするためordered_numberを足す
+  ordered_number = gets.to_i
+  order_number = ordered_number - 1
   puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
 end
@@ -29,6 +32,7 @@ order1 = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
 order2 = take_order(FOODS)
-
-total = FOODS[order1][:price] + DRINKS[order2][:price]
+# FOODSとDRINKSが反対だったので入れ替える
+# 数値が文字列だったので.to_iを足す
+total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
