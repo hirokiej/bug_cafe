e078ce77c9ebdf0d69b7f57ada4eea39c37993fc
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-01-29 12:43:20 -0500

    order_numberとorder1と2の修正をしました。

--- cafe.rb
+++ cafe.rb
@@ -19,19 +19,17 @@ def take_order(menus)
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
   print '>'
-  # 正しい配列出力にするためordered_numberを足す
-  ordered_number = gets.to_i
-  order_number = ordered_number - 1
+  order_number = gets.to_i - 1
   puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drink_order = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+food_order = take_order(FOODS)
 # FOODSとDRINKSが反対だったので入れ替える
 # 数値が文字列だったので.to_iを足す
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+total = DRINKS[drink_order][:price].to_i + FOODS[food_order][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
